d49e6a176d0ee992d5f6c2c2c3707d1126a86a34
br3-mah <nyeleti.bremah@gmail.com>
2025-04-14 11:56:11 +0200

    cross communication admin to app

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -58,8 +58,8 @@ return [
         'custom_public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            // 'url' => env('APP_CUSTOM_STORAGE_URL', 'https://app.mightyfinance.co.zm/public/storage'),
-            'url' => env('APP_CUSTOM_STORAGE_URL', 'http://localhost/mfs-app/public/storage'),
+            'url' => env('APP_CUSTOM_STORAGE_URL', 'https://app.mightyfinance.co.zm/public/storage'),
+            // 'url' => env('APP_CUSTOM_STORAGE_URL', 'http://localhost/mfs-app/public/storage'),
             'visibility' => 'public',
         ],
 
