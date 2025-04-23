dd39d5c582c5f7f64bba792f1fae3ce0ccd65e8e
br3-mah <nyeleti.bremah@gmail.com>
2025-04-23 12:39:43 +0200

    Breadcrums

--- resources/views/livewire/dashboard/loans/no-repayments.blade.php
+++ resources/views/livewire/dashboard/loans/no-repayments.blade.php
@@ -1,16 +1,16 @@
 <div class="content">
-    <div class="row">
-        <div class="col-12">
-            @if(!empty($loan_requests?->toArray()))
-                @include('livewire.dashboard.loans.__parts.staff-loan-request-table')
-            @else
-                <div class="container m-12 d-flex justify-content-center align-items-center">
-                    <div class="text-center col-12">
-                        <img width="300" src="{{ asset('public/mfs/admin/assets/media/illustrations/sigma-1/loan.png')}}" alt="">
-                        <p>Empty</p>
-                    </div>
+    @include('livewire.dashboard.loans.__parts.index-loan-crum')
+    {{-- @dd($loan_requests?->toArray()) --}}
+    <div class="col-12">
+        @if(!empty($loan_requests?->toArray()))
+            @include('livewire.dashboard.loans.__parts.staff-loan-request-table')
+        @else
+            <div class="container m-12 d-flex justify-content-center align-items-center">
+                <div class="text-center col-12">
+                    <img width="300" src="{{ asset('public/mfs/admin/assets/media/illustrations/sigma-1/loan.png')}}" alt="">
+                    <p>Empty</p>
                 </div>
-            @endif
-        </div>
+            </div>
+        @endif
     </div>
 </div>
